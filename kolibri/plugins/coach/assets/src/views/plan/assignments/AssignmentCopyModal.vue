5ec9d6fedda293384c8978f4c05f5c76ede0502f
Jonathan Boiser <jboiser@gmail.com>
2020-08-12 13:42:08 -0700

    Add two states
    Move more estuff to machine

--- kolibri/plugins/coach/assets/src/views/plan/assignments/AssignmentCopyModal.vue
+++ kolibri/plugins/coach/assets/src/views/plan/assignments/AssignmentCopyModal.vue
@@ -3,27 +3,38 @@
   <KModal
     :title="modalTitle"
     v-bind="modalTexts"
+    :submitDisabled="blockControls"
     @submit="handleSubmit"
     @cancel="$emit('cancel')"
   >
     <!-- Classroom Selection Form -->
-    <div v-if="stage === Stages.SELECT_CLASSROOM" id="select-classroom">
+    <div v-if="atSelectClassroom" id="select-classroom">
       <KRadioButton
         v-for="classroom in availableClassrooms"
         :key="classroom.id"
         v-model="selectedClassroomId"
         :label="classroomLabel(classroom)"
         :value="classroom.id"
+        :disabled="blockControls"
         data-test="radio-button"
       />
     </div>
+
     <!-- Learner Group Selection Form -->
-    <div v-else id="select-learnergroup">
+    <div v-if="current.matches('SELECT_GROUPS')" id="select-learnergroup">
+      <div v-if="current.matches('SELECT_GROUPS.LOADING')">
+        <h1>Loading</h1>
+      </div>
+      <div v-else-if="current.matches('SELECT_GROUPS.FAILED')">
+        <h1>Failed</h1>
+      </div>
+
       <p>{{ $tr('destinationExplanation', { classroomName: selectedClassroomName }) }}</p>
       <p>{{ assignmentQuestion }}</p>
       <RecipientSelector
+        v-if="current.matches('SELECT_GROUPS.SUCCESS')"
         v-model="selectedCollectionIds"
-        :groups="availableGroups"
+        :groups="context.groups"
         :classId="selectedClassroomId"
         :initialAdHocLearners="[]"
         data-test="recipient-selector"
@@ -37,6 +48,7 @@
 
 <script>
 
+  import { createMachine, interpret, assign } from 'xstate';
   import sortBy from 'lodash/sortBy';
   import find from 'lodash/find';
   import { error as logError } from 'kolibri.lib.logging';
@@ -45,10 +57,52 @@
   import { coachStringsMixin } from '../../common/commonCoachStrings';
   import RecipientSelector from './RecipientSelector';
 
-  const Stages = {
-    SELECT_CLASSROOM: 'SELECT_CLASSROOM',
-    SELECT_GROUPS: 'SELECT_GROUPS',
-  };
+  const copyModalMachine = () =>
+    createMachine({
+      id: 'assignmentcopying',
+      initial: 'SELECT_CLASSROOM',
+      context: {
+        groups: [],
+      },
+      states: {
+        SELECT_CLASSROOM: {
+          on: {
+            fetchgroups: 'SELECT_GROUPS',
+          },
+        },
+        SELECT_GROUPS: {
+          // Event: { type: SELECT_GROUPS, classroomId }
+          initial: 'GROUPS_LOADING',
+          states: {
+            GROUPS_LOADING: {
+              invoke: {
+                src(context, event) {
+                  return LearnerGroupResource.fetchCollection({
+                    getParams: { parent: event.classroomId },
+                  });
+                },
+                onDone: {
+                  target: 'GROUPS_OK',
+                  actions: assign({ groups: (context, event) => event.data }),
+                },
+                onError: {
+                  target: 'GROUPS_FAIL',
+                },
+              },
+            },
+            GROUPS_OK: {},
+            GROUPS_FAIL: {},
+          },
+          on: {
+            // TODO move Assignment API call here
+            submit: 'SUCCESS',
+          },
+        },
+        SUCCESS: {
+          type: 'final',
+        },
+      },
+    });
 
   export default {
     name: 'AssignmentCopyModal',
@@ -76,12 +130,13 @@
     },
     data() {
       return {
-        Stages,
-        availableGroups: [],
-        blockControls: false,
+        // Machine
+        machine: null,
+        current: null,
+        context: null,
+        // Form state
         selectedClassroomId: null,
         selectedCollectionIds: [],
-        stage: Stages.SELECT_CLASSROOM,
         adHocLearners: [],
       };
     },
@@ -92,12 +147,18 @@
         }
         return find(this.classList, { id: this.selectedClassroomId }).name;
       },
+      atSelectClassroom() {
+        return this.current.matches('SELECT_CLASSROOM');
+      },
+      blockControls() {
+        return this.current.matches('SELECT_GROUPS.LOADING');
+      },
       availableClassrooms() {
         // put current classroom on the top
         return sortBy(this.classList, classroom => (this.isCurrentClassroom(classroom) ? -1 : 1));
       },
       modalTexts() {
-        if (this.stage === this.Stages.SELECT_CLASSROOM) {
+        if (this.atSelectClassroom) {
           return {
             submitText: this.coreString('continueAction'),
             cancelText: this.coreString('cancelAction'),
@@ -111,42 +172,37 @@
     },
     created() {
       this.selectedClassroomId = this.classId;
+      // Start service on component creation
+      this.setupMachine();
     },
     methods: {
-      getLearnerGroupsForClassroom(classroomId) {
-        return LearnerGroupResource.fetchCollection({ getParams: { parent: classroomId } });
+      setupMachine() {
+        this.machine = interpret(copyModalMachine());
+        const { initialState, context } = this.machine;
+        this.current = initialState;
+        this.context = context;
+        this.machine
+          .onTransition(state => {
+            this.current = state;
+            this.context = state.context;
+          })
+          .onDone(() => this.emitSubmit())
+          .start();
       },
       handleSubmit() {
-        if (this.stage === this.Stages.SELECT_CLASSROOM) {
-          this.goToAvailableGroups();
+        if (this.atSelectClassroom) {
+          this.machine.send({ type: 'fetchgroups', classroomId: this.selectedClassroomId });
         } else {
-          this.$emit(
-            'submit',
-            this.selectedClassroomId,
-            this.selectedCollectionIds,
-            this.adHocLearners
-          );
+          this.machine.send({ type: 'submit' });
         }
       },
-      goToAvailableGroups() {
-        // Do nothing if user presses Continue more than once
-        if (this.blockControls) {
-          return;
-        }
-        this.blockControls = true;
-        // Select Entire Classroom by default
-        this.selectedCollectionIds = [this.selectedClassroomId];
-        return this.getLearnerGroupsForClassroom(this.selectedClassroomId)
-          .then(groups => {
-            this.availableGroups = groups;
-            this.stage = Stages.SELECT_GROUPS;
-            this.blockControls = false;
-          })
-          .catch(error => {
-            this.$store.dispatch('handleApiError', error);
-            logError(error);
-            this.blockControls = false;
-          });
+      emitSubmit() {
+        this.$emit(
+          'submit',
+          this.selectedClassroomId,
+          this.selectedCollectionIds,
+          this.adHocLearners
+        );
       },
       classroomLabel(classroom) {
         if (this.isCurrentClassroom(classroom)) {
